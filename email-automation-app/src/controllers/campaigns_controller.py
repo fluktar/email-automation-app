c2b9ab8ac7e6ed2cf6fd5cb799cec2b4bdb076dd
fluktar <tyc.pawel@gmail.com>
2025-07-07 09:16:49 +0200

    Dodaj kontroler kampanii oraz kontroler kontaktów z funkcjami inicjalizacji i zarządzania danymi

---
+++ email-automation-app/src/controllers/campaigns_controller.py
@@ -0,0 +1,18 @@
+class CampaignsController:
+    def __init__(self, gui):
+        self.gui = gui
+
+    def add_campaign(self):
+        pass
+
+    def remove_campaign(self):
+        pass
+
+    def load_campaigns(self):
+        pass
+
+    def on_campaign_select_messages(self, event):
+        pass
+
+    def init_messages_tab(self):
+        pass
